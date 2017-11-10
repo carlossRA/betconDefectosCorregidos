bff501cf6bc80cf56f11c9f063e23cd7fb85541f
carlossRA <Carlos.Rodriguez24@alu.uclm.es>
2017-11-10 16:37:17 +0100

    Update new_competition.py

--- src/new_competition.py
+++ src/new_competition.py
@@ -8,7 +8,7 @@ from competitions import Competitions
 
 class NewCompetition(QWidget):
 	def __init__(self, mainWindows):
-		QWidget.__init__(self)
+	       #QWidget.__init__(self)
 		uic.loadUi(directory + "/../ui/new_competition.ui", self)
 		self.mainWindows = mainWindows
 		self.btnAccept.clicked.connect(self.accept)

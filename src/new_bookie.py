937daf932f6158db53f7dbb987618cd05eaca179
carlossRA <Carlos.Rodriguez24@alu.uclm.es>
2017-11-10 16:36:38 +0100

    Update new_bookie.py

--- src/new_bookie.py
+++ src/new_bookie.py
@@ -9,7 +9,7 @@ from bookie import Bookie
 
 class NewBookie(QWidget):
     def __init__(self, mainWindows):
-        QWidget.__init__(self)
+      # QWidget.__init__(self)
         uic.loadUi(directory + "/../ui/new_bookie.ui", self)
         self.mainWindows = mainWindows
         self.btnAccept.clicked.connect(self.accept)
